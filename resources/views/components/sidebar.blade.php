4df19f85cfef554a3e983c8abe6c64a15473f402
SLarkZ01 <tmontoyamagon11@gmail.com>
2025-04-12 23:16:48 -0500

    ultimos cambios en la mejora del sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -31,7 +31,7 @@
                 "families": ["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands",
                     "simple-line-icons"
                 ],
-                urls: ["{{ asset('css/sidebar/fonts.css') }}"]
+                urls: ["{{ asset('css/sidebar/fonts.min.css') }}"]
             },
             active: function() {
                 sessionStorage.fonts = true;
